eff148ccf680f804d1900a7354ad3d052c1e37da
fragilefort <imaddex3759@gmail.com>
2026-06-11 14:15:02 +0200

    filtering sam headers

--- crates/sam_to_map/src/main.rs
+++ crates/sam_to_map/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader};
 fn main() -> std::io::Result<()> {
     let f = File::open("sample.sam").expect("Couldn't open sam file");
     let reader = BufReader::new(f);
-    let lines_iter = reader.lines().map(|l| l.unwrap());
+    let mut lines_iter = reader.lines().map(|l| l.unwrap());
+    lines_iter.next().filter(|line| line.starts_with("@"));
     Ok(())
 }
