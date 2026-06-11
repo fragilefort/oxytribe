3e6972135f22efa12129e260a7b95084ac45ccd8
fragilefort <imaddex3759@gmail.com>
2026-06-11 12:38:29 +0200

    Defined the BuffReader

--- crates/sam_to_map/src/main.rs
+++ crates/sam_to_map/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::BufReader;
+
+fn main() -> std::io::Result<()> {
+    let f = File::open("sample.sam").expect("Couldn't open sam file");
+    let mut reader = BufReader::new(f);
+
+    Ok(())
 }
