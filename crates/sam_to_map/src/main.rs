90bd05c1634b51bdd94edc685f79d7b0e5f42d83
fragilefort <imaddex3759@gmail.com>
2026-06-11 21:20:54 +0200

    One step closer to parse the sam files: seprate fields and extract  each field

--- crates/sam_to_map/src/main.rs
+++ crates/sam_to_map/src/main.rs
@@ -7,6 +7,14 @@ fn main() -> std::io::Result<()> {
     let lines_iter = reader
         .lines()
         .map(|l| l.unwrap())
-        .filter(|line| !line.starts_with("@"));
+        .filter(|line| !line.starts_with("@"))
+        .map(|line| {
+            let fields: Vec<&str> = line.split('\t').collect();
+            let start = fields[3].parse::<u32>();
+            let cigar = fields[5];
+            let sequence = fields[9];
+            let md_tag = extract_md_tag(&fields);
+        });
+
     Ok(())
 }
