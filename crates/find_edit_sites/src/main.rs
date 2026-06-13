77f8fae8ec611961aa6cc0bb9598e4fd1c033609
fragilefort <imaddex3759@gmail.com>
2026-06-14 00:56:01 +0200

    Added cl arguments for find_edit_sites

--- crates/find_edit_sites/src/main.rs
+++ crates/find_edit_sites/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Cli {
+    rna_path: PathBuf,
+    control_path: PathBuf,
+    output_path: PathBuf,
+    #[arg(long, default_value_t = 9)]
+    min_control_coverage: u32,
+    /// Maximum allowed edit (G) fraction in the control sample (filters out SNPs)
+    #[arg(long, default_value_t = 0.005)]
+    max_control_edit_frac: f64,
+    #[arg(long, default_value_t = 0.8)]
+    min_control_nonedit_frac: f64,
+    /// Minimum total coverage required at this position in the RNA sample
+    #[arg(long, default_value_t = 20)]
+    min_rna_coverage: u32,
+    /// Minimum edit (G) fraction required in the RNA sample
+    #[arg(long, default_value_t = 0.05)]
+    min_rna_edit_frac: f64,
+}
+fn main() -> std::io::Result<()> {
+    Ok(())
 }
