477e9d9f46408ee6f72357e6297c25ab8d38ac38
fragilefort <imaddex3759@gmail.com>
2026-06-18 12:16:52 +0200

    Feat: read input files and mmap into each of them

--- crates/combine_edit_sites/src/main.rs
+++ crates/combine_edit_sites/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use memmap2::Mmap;
+use std::fs::File;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Cli {
+    /// Input binary files (one per replicate)
+    inputs: Vec<PathBuf>,
+    /// Output binary file
+    #[arg(long)]
+    output: PathBuf,
+    /// Combination mode: and/or
+    #[arg(long)]
+    mode: Mode,
+}
+
+#[derive(clap::ValueEnum, Clone)]
+enum Mode {
+    And,
+    Or,
+}
+
+fn main() -> std::io::Result<()> {
+    let cli = Cli::parse();
+    let files: Vec<File> = cli
+        .inputs
+        .iter()
+        .map(|path| File::open(&path).expect("Cannot open input file"))
+        .collect();
+    // SAFETY: input files is written once by sam_to_map and not modified
+    let mmaps: Vec<Mmap> = files
+        .iter()
+        .map(|f| unsafe { Mmap::map(f).expect("couldn't mmap file") })
+        .collect();
+    Ok(())
 }
